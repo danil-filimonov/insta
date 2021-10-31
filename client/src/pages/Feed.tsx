2f7f336697ef85cc8155de1b1e66a28b1c0afdb3
Danil Filimonov <lsamba99@gmail.com>
2021-11-01 02:29:04 +0700

    добавляю первоначальное отображение карточек постов в ленте

--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import { Card, Avatar, Image } from 'antd'
+
+import noImage from '../assets/images/no-image.jpg'
+
 const Feed: React.FC = () => {
+  const { Meta } = Card
+
+  const containerStyles = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    columnGap: '15px',
+    rowGap: '30px',
+    padding: '45px 0'
+  }
+
   return (
     <section className='feed'>
-      <div className='container'>Feed</div>
+      <div className='container' style={containerStyles}>
+        <Card cover={<Image src={'error'} fallback={noImage} />}>
+          <Meta title='username' description='Some post text' avatar={<Avatar src='https://joeschmoe.io/api/v1/random' />} />
+        </Card>
+      </div>
     </section>
   )
 }
